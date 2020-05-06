d5b4f78b14341487bffd61fcf3329e369f376bcb
Josh Adams <joshua.william.adams@gmail.com>
2020-05-07 07:49:14 +0800

    fix-linting-on-changes

--- src/js/modalator.js
+++ src/js/modalator.js
@@ -38,19 +38,19 @@
 const Modal = function Modal(config, dialog_body, dialog_button) {
   const _this = this;
   _this.config = config;
-  _this.dialog_title = "";
-  _this.dialog_body = "";
-  _this.dialog_button = "";
-  
+  _this.dialog_title = '';
+  _this.dialog_body = '';
+  _this.dialog_button = '';
+
   if (typeof (config) === 'object') {
     _this.dialog_title = _this.config.dialog.header.title.text().outerHTML;
-    if (typeof _this.config.dialog.header.title.text() === "string") {
-		  _this.dialog_title = _this.config.dialog.header.title.text();
-	  }  
+    if (typeof _this.config.dialog.header.title.text() === 'string') {
+      _this.dialog_title = _this.config.dialog.header.title.text();
+    }
     _this.dialog_body = _this.config.dialog.body.content.text().outerHTML;
-    if (typeof _this.config.dialog.body.content.text() === "string") {
-		  _this.dialog_body = _this.config.dialog.body.content.text();
-	  } 
+    if (typeof _this.config.dialog.body.content.text() === 'string') {
+      _this.dialog_body = _this.config.dialog.body.content.text();
+    }
     _this.dialog_button_one = _this.config.dialog.footer.buttons.button_one.text;
     if (!(typeof _this.isDefined(_this.config, 'dialog.footer.buttons.button_two.text') === 'undefined')) {
       _this.dialog_button_two = _this.config.dialog.footer.buttons.button_two.text;
@@ -115,13 +115,13 @@ const Modal = function Modal(config, dialog_body, dialog_button) {
       'font-weight': '500',
       'color': '#212529',
       'font-size': '20px',
-	    'float': 'left'
+      'float': 'left'
     },
-    dialog_header_close_icon_style: {      
+    dialog_header_close_icon_style: {
       'position': 'absolute',
-	    'top': '0px',
-	    'right': '0px',
-	    'padding': '4px 12px',
+      'top': '0px',
+      'right': '0px',
+      'padding': '4px 12px',
       'color': '#C5C5C5',
       'font-size': '20px',
       'font-weight': '700',
@@ -233,18 +233,17 @@ const Modal = function Modal(config, dialog_body, dialog_button) {
     for (const style in _this.config.dialog.header.close_icon.style()) {
       data.dialog_header_close_icon.style.cssText = data.dialog_header_close_icon.style.cssText.concat(style + ' : ' + _this.config.dialog.header.close_icon.style()[style] + ';');
     }
+  } // for loop for adding style object to the dialog header text
 
-  } //for loop for adding style object to the dialog header	text	
-  
-	//code to make the hover function over close x button
-	data.dialog_header_close_icon.setAttribute('data-color',data.dialog_header_close_icon.style.color);
-	data.dialog_header_close_icon.onmouseover = function(){		
-		data.dialog_header_close_icon.style.color= _this.adjust_brightness(_this.getRGBCode(data.dialog_header_close_icon.style.color,_this),16);				
-	};
-	data.dialog_header_close_icon.onmouseout = function(){		
-		data.dialog_header_close_icon.style.color= data.dialog_header_close_icon.getAttribute('data-color');		
-	};
-	//code to make the hover function over close x button
+  // code to make the hover function over close x button
+  data.dialog_header_close_icon.setAttribute('data-color', data.dialog_header_close_icon.style.color);
+  data.dialog_header_close_icon.onmouseover = function () {
+    data.dialog_header_close_icon.style.color = _this.adjust_brightness(_this.getRGBCode(data.dialog_header_close_icon.style.color, _this), 16);
+  };
+  data.dialog_header_close_icon.onmouseout = function () {
+    data.dialog_header_close_icon.style.color = data.dialog_header_close_icon.getAttribute('data-color');
+  };
+  // code to make the hover function over close x button
 
   for (const style in data.dialog_body_style) {
     data.dialog_body.style.cssText = data.dialog_body.style.cssText.concat(style + ' : ' + data.dialog_body_style[style] + ';');
@@ -284,16 +283,16 @@ const Modal = function Modal(config, dialog_body, dialog_button) {
     }
   }
 
-	//code to make the hover function
-	data.dialog_footer_button_one.setAttribute('data-background',data.dialog_footer_button_one.style.background);
-	data.dialog_footer_button_one.onmouseover = function(){		
-		data.dialog_footer_button_one.style.background= _this.adjust_brightness(_this.getRGBCode(data.dialog_footer_button_one.style.background,_this),16);				
-	};
-	data.dialog_footer_button_one.onmouseout = function(){		
-		data.dialog_footer_button_one.style.background= data.dialog_footer_button_one.getAttribute('data-background');		
-	};
-	//end of code to make the hover function
-	
+  // code to make the hover function
+  data.dialog_footer_button_one.setAttribute('data-background', data.dialog_footer_button_one.style.background);
+  data.dialog_footer_button_one.onmouseover = function () {
+    data.dialog_footer_button_one.style.background= _this.adjust_brightness(_this.getRGBCode(data.dialog_footer_button_one.style.background, _this), 16);
+  };
+  data.dialog_footer_button_one.onmouseout = function () {
+    data.dialog_footer_button_one.style.background= data.dialog_footer_button_one.getAttribute('data-background');
+  };
+  // end of code to make the hover function
+
   if (!(typeof this.isDefined(_this.config, 'dialog.footer.buttons.button_two.text') === 'undefined')) {
     // for loop for adding style object to the dialog header text
     for (const style in data.dialog_footer_button_two_style) {
@@ -305,16 +304,16 @@ const Modal = function Modal(config, dialog_body, dialog_button) {
     }
 
     data.dialog_footer_button_two.innerHTML = _this.dialog_button_two;
-	
-	//code to make the hover function
-	data.dialog_footer_button_two.setAttribute('data-background',data.dialog_footer_button_two.style.background);	
-	data.dialog_footer_button_two.onmouseover = function(){		
-		data.dialog_footer_button_two.style.background= _this.adjust_brightness(_this.getRGBCode(data.dialog_footer_button_two.style.background,_this),16);				
-	};
-	data.dialog_footer_button_two.onmouseout = function(){		
-		data.dialog_footer_button_two.style.background= data.dialog_footer_button_two.getAttribute('data-background');		
-	};
-	//end of code to make the hover function
+
+    // code to make the hover function
+    data.dialog_footer_button_two.setAttribute('data-background', data.dialog_footer_button_two.style.background);
+    data.dialog_footer_button_two.onmouseover = function () {
+      data.dialog_footer_button_two.style.background= _this.adjust_brightness(_this.getRGBCode(data.dialog_footer_button_two.style.background, _this), 16);
+    };
+    data.dialog_footer_button_two.onmouseout = function () {
+      data.dialog_footer_button_two.style.background= data.dialog_footer_button_two.getAttribute('data-background');
+    };
+    // end of code to make the hover function
     data.dialog_footer_button_two.onclick = function () {
       _this.config.dialog.footer.buttons.button_two.run();
     };
@@ -377,57 +376,174 @@ Modal.prototype.isDefined = function isDefined(_this, key) {
   }, _this);
 };
 
-//Function to convert rgb color to hex format
-Modal.prototype.getRGBCode = function(rgb_code,_this) {
-	if(rgb_code.includes("#")){
-		return rgb_code;
-	} else if(rgb_code.includes("rgb")){
-		rgb_code = rgb_code.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-		return "#" + _this.getHex(rgb_code[1],_this) + _this.getHex(rgb_code[2]) + _this.getHex(rgb_code[3]);
-	} else{
-		return _this.getHexCodeFromColor(rgb_code);
-	}
-}
+// Function to convert rgb color to hex format
+Modal.prototype.getRGBCode = function (rgb_code, _this) {
+  if (rgb_code.includes('#')) {
+    return rgb_code;
+  } else if (rgb_code.includes('rgb')) {
+    rgb_code = rgb_code.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+    return '#' + _this.getHex(rgb_code[1], _this) + _this.getHex(rgb_code[2]) + _this.getHex(rgb_code[3]);
+  } else {
+    return _this.getHexCodeFromColor(rgb_code);
+  }
+};
 Modal.prototype.getHex = function (x) {
-	var hexDigits = new Array("0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f");
-	return isNaN(x) ? "00" : hexDigits[(x - x % 16) / 16] + hexDigits[x % 16];
- }
-Modal.prototype.adjust_brightness = function (hex_code, percent){
-    var r = parseInt(hex_code.substr(1, 2), 16),
-        g = parseInt(hex_code.substr(3, 2), 16),
-        b = parseInt(hex_code.substr(5, 2), 16);
-   return '#' + ((0|(1<<8) + r * (100 - percent) / 100).toString(16)).substr(1) + ((0|(1<<8) + g * (100 - percent) / 100).toString(16)).substr(1) + ((0|(1<<8) + b * (100 - percent) / 100).toString(16)).substr(1);
-}
-Modal.prototype.getHexCodeFromColor = function (color_name){
-    var colours = {"aliceblue":"#f0f8ff","antiquewhite":"#faebd7","aqua":"#00ffff","aquamarine":"#7fffd4","azure":"#f0ffff",
-    "beige":"#f5f5dc","bisque":"#ffe4c4","black":"#000000","blanchedalmond":"#ffebcd","blue":"#0000ff","blueviolet":"#8a2be2","brown":"#a52a2a","burlywood":"#deb887",
-    "cadetblue":"#5f9ea0","chartreuse":"#7fff00","chocolate":"#d2691e","coral":"#ff7f50","cornflowerblue":"#6495ed","cornsilk":"#fff8dc","crimson":"#dc143c","cyan":"#00ffff",
-    "darkblue":"#00008b","darkcyan":"#008b8b","darkgoldenrod":"#b8860b","darkgray":"#a9a9a9","darkgreen":"#006400","darkkhaki":"#bdb76b","darkmagenta":"#8b008b","darkolivegreen":"#556b2f",
-    "darkorange":"#ff8c00","darkorchid":"#9932cc","darkred":"#8b0000","darksalmon":"#e9967a","darkseagreen":"#8fbc8f","darkslateblue":"#483d8b","darkslategray":"#2f4f4f","darkturquoise":"#00ced1",
-    "darkviolet":"#9400d3","deeppink":"#ff1493","deepskyblue":"#00bfff","dimgray":"#696969","dodgerblue":"#1e90ff",
-    "firebrick":"#b22222","floralwhite":"#fffaf0","forestgreen":"#228b22","fuchsia":"#ff00ff",
-    "gainsboro":"#dcdcdc","ghostwhite":"#f8f8ff","gold":"#ffd700","goldenrod":"#daa520","gray":"#808080","green":"#008000","greenyellow":"#adff2f",
-    "honeydew":"#f0fff0","hotpink":"#ff69b4",
-    "indianred ":"#cd5c5c","indigo":"#4b0082","ivory":"#fffff0","khaki":"#f0e68c",
-    "lavender":"#e6e6fa","lavenderblush":"#fff0f5","lawngreen":"#7cfc00","lemonchiffon":"#fffacd","lightblue":"#add8e6","lightcoral":"#f08080","lightcyan":"#e0ffff","lightgoldenrodyellow":"#fafad2",
-    "lightgrey":"#d3d3d3","lightgreen":"#90ee90","lightpink":"#ffb6c1","lightsalmon":"#ffa07a","lightseagreen":"#20b2aa","lightskyblue":"#87cefa","lightslategray":"#778899","lightsteelblue":"#b0c4de",
-    "lightyellow":"#ffffe0","lime":"#00ff00","limegreen":"#32cd32","linen":"#faf0e6",
-    "magenta":"#ff00ff","maroon":"#800000","mediumaquamarine":"#66cdaa","mediumblue":"#0000cd","mediumorchid":"#ba55d3","mediumpurple":"#9370d8","mediumseagreen":"#3cb371","mediumslateblue":"#7b68ee",
-    "mediumspringgreen":"#00fa9a","mediumturquoise":"#48d1cc","mediumvioletred":"#c71585","midnightblue":"#191970","mintcream":"#f5fffa","mistyrose":"#ffe4e1","moccasin":"#ffe4b5",
-    "navajowhite":"#ffdead","navy":"#000080",
-    "oldlace":"#fdf5e6","olive":"#808000","olivedrab":"#6b8e23","orange":"#ffa500","orangered":"#ff4500","orchid":"#da70d6",
-    "palegoldenrod":"#eee8aa","palegreen":"#98fb98","paleturquoise":"#afeeee","palevioletred":"#d87093","papayawhip":"#ffefd5","peachpuff":"#ffdab9","peru":"#cd853f","pink":"#ffc0cb","plum":"#dda0dd","powderblue":"#b0e0e6","purple":"#800080",
-    "rebeccapurple":"#663399","red":"#ff0000","rosybrown":"#bc8f8f","royalblue":"#4169e1",
-    "saddlebrown":"#8b4513","salmon":"#fa8072","sandybrown":"#f4a460","seagreen":"#2e8b57","seashell":"#fff5ee","sienna":"#a0522d","silver":"#c0c0c0","skyblue":"#87ceeb","slateblue":"#6a5acd","slategray":"#708090","snow":"#fffafa","springgreen":"#00ff7f","steelblue":"#4682b4",
-    "tan":"#d2b48c","teal":"#008080","thistle":"#d8bfd8","tomato":"#ff6347","turquoise":"#40e0d0",
-    "violet":"#ee82ee",
-    "wheat":"#f5deb3","white":"#ffffff","whitesmoke":"#f5f5f5",
-    "yellow":"#ffff00","yellowgreen":"#9acd32"};
-    if (typeof colours[color_name.toLowerCase()] != 'undefined')
-        return colours[color_name.toLowerCase()];
-
-    return false;
-}
+  const hexDigits = new Array('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f');
+  return isNaN(x) ? '00' : hexDigits[(x - x % 16) / 16] + hexDigits[x % 16];
+};
+Modal.prototype.adjust_brightness = function (hex_code, percent) {
+  const r = parseInt(hex_code.substr(1, 2), 16);
+  const g = parseInt(hex_code.substr(3, 2), 16);
+  const b = parseInt(hex_code.substr(5, 2), 16);
+  return '#' + ((0|(1<<8) + r * (100 - percent) / 100).toString(16)).substr(1) + ((0|(1<<8) + g * (100 - percent) / 100).toString(16)).substr(1) + ((0|(1<<8) + b * (100 - percent) / 100).toString(16)).substr(1);
+};
+Modal.prototype.getHexCodeFromColor = function (color_name) {
+  const colours = {
+    'aliceblue': '#f0f8ff',
+    'antiquewhite': '#faebd7',
+    'aqua': '#00ffff',
+    'aquamarine': '#7fffd4',
+    'azure': '#f0ffff',
+    'beige': '#f5f5dc',
+    'bisque': '#ffe4c4',
+    'black': '#000000',
+    'blanchedalmond': '#ffebcd',
+    'blue': '#0000ff',
+    'blueviolet': '#8a2be2',
+    'brown': '#a52a2a',
+    'burlywood': '#deb887',
+    'cadetblue': '#5f9ea0',
+    'chartreuse': '#7fff00',
+    'chocolate': '#d2691e',
+    'coral': '#ff7f50',
+    'cornflowerblue': '#6495ed',
+    'cornsilk': '#fff8dc',
+    'crimson': '#dc143c',
+    'cyan': '#00ffff',
+    'darkblue': '#00008b',
+    'darkcyan': '#008b8b',
+    'darkgoldenrod': '#b8860b',
+    'darkgray': '#a9a9a9',
+    'darkgreen': '#006400',
+    'darkkhaki': '#bdb76b',
+    'darkmagenta': '#8b008b',
+    'darkolivegreen': '#556b2f',
+    'darkorange': '#ff8c00',
+    'darkorchid': '#9932cc',
+    'darkred': '#8b0000',
+    'darksalmon': '#e9967a',
+    'darkseagreen': '#8fbc8f',
+    'darkslateblue': '#483d8b',
+    'darkslategray': '#2f4f4f',
+    'darkturquoise': '#00ced1',
+    'darkviolet': '#9400d3',
+    'deeppink': '#ff1493',
+    'deepskyblue': '#00bfff',
+    'dimgray': '#696969',
+    'dodgerblue': '#1e90ff',
+    'firebrick': '#b22222',
+    'floralwhite': '#fffaf0',
+    'forestgreen': '#228b22',
+    'fuchsia': '#ff00ff',
+    'gainsboro': '#dcdcdc',
+    'ghostwhite': '#f8f8ff',
+    'gold': '#ffd700',
+    'goldenrod': '#daa520',
+    'gray': '#808080',
+    'green': '#008000',
+    'greenyellow': '#adff2f',
+    'honeydew': '#f0fff0',
+    'hotpink': '#ff69b4',
+    'indianred ': '#cd5c5c',
+    'indigo': '#4b0082',
+    'ivory': '#fffff0',
+    'khaki': '#f0e68c',
+    'lavender': '#e6e6fa',
+    'lavenderblush': '#fff0f5',
+    'lawngreen': '#7cfc00',
+    'lemonchiffon': '#fffacd',
+    'lightblue': '#add8e6',
+    'lightcoral': '#f08080',
+    'lightcyan': '#e0ffff',
+    'lightgoldenrodyellow': '#fafad2',
+    'lightgrey': '#d3d3d3',
+    'lightgreen': '#90ee90',
+    'lightpink': '#ffb6c1',
+    'lightsalmon': '#ffa07a',
+    'lightseagreen': '#20b2aa',
+    'lightskyblue': '#87cefa',
+    'lightslategray': '#778899',
+    'lightsteelblue': '#b0c4de',
+    'lightyellow': '#ffffe0',
+    'lime': '#00ff00',
+    'limegreen': '#32cd32',
+    'linen': '#faf0e6',
+    'magenta': '#ff00ff',
+    'maroon': '#800000',
+    'mediumaquamarine': '#66cdaa',
+    'mediumblue': '#0000cd',
+    'mediumorchid': '#ba55d3',
+    'mediumpurple': '#9370d8',
+    'mediumseagreen': '#3cb371',
+    'mediumslateblue': '#7b68ee',
+    'mediumspringgreen': '#00fa9a',
+    'mediumturquoise': '#48d1cc',
+    'mediumvioletred': '#c71585',
+    'midnightblue': '#191970',
+    'mintcream': '#f5fffa',
+    'mistyrose': '#ffe4e1',
+    'moccasin': '#ffe4b5',
+    'navajowhite': '#ffdead',
+    'navy': '#000080',
+    'oldlace': '#fdf5e6',
+    'olive': '#808000',
+    'olivedrab': '#6b8e23',
+    'orange': '#ffa500',
+    'orangered': '#ff4500',
+    'orchid': '#da70d6',
+    'palegoldenrod': '#eee8aa',
+    'palegreen': '#98fb98',
+    'paleturquoise': '#afeeee',
+    'palevioletred': '#d87093',
+    'papayawhip': '#ffefd5',
+    'peachpuff': '#ffdab9',
+    'peru': '#cd853f',
+    'pink': '#ffc0cb',
+    'plum': '#dda0dd',
+    'powderblue': '#b0e0e6',
+    'purple': '#800080',
+    'rebeccapurple': '#663399',
+    'red': '#ff0000',
+    'rosybrown': '#bc8f8f',
+    'royalblue': '#4169e1',
+    'saddlebrown': '#8b4513',
+    'salmon': '#fa8072',
+    'sandybrown': '#f4a460',
+    'seagreen': '#2e8b57',
+    'seashell': '#fff5ee',
+    'sienna': '#a0522d',
+    'silver': '#c0c0c0',
+    'skyblue': '#87ceeb',
+    'slateblue': '#6a5acd',
+    'slategray': '#708090',
+    'snow': '#fffafa',
+    'springgreen': '#00ff7f',
+    'steelblue': '#4682b4',
+    'tan': '#d2b48c',
+    'teal': '#008080',
+    'thistle': '#d8bfd8',
+    'tomato': '#ff6347',
+    'turquoise': '#40e0d0',
+    'violet': '#ee82ee',
+    'wheat': '#f5deb3',
+    'white': '#ffffff',
+    'whitesmoke': '#f5f5f5',
+    'yellow': '#ffff00',
+    'yellowgreen': '#9acd32'
+  };
+  if (typeof colours[color_name.toLowerCase()] !== 'undefined') return colours[color_name.toLowerCase()];
+  return false;
+};
 
 /* =========================== Export Public APIs =========================== */
 
